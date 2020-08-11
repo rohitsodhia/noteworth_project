897a723cb1e7ba84782d95576042b2f8f0adfaea
Rohit Sodhia <sodhia.rohit@gmail.com>
2020-08-11 00:10:34 -0400

    Show auth token on landing page

--- landing/views.py
+++ landing/views.py
@@ -1,7 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from authtokens.models import AuthToken
+
 # Create your views here.
 def landing(request):
-    return render(request, "landing.html", {})
+    context = {"last_token": None}
+    try:
+        auth_token = AuthToken.objects.filter().order_by("-createdAt")[0]
+    except IndexError:
+        pass
+    else:
+        context["last_token"] = auth_token.createdAt
+    return render(request, "landing.html", context)
 
