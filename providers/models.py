fd26631e347d9315c2e2297ae59d66c76ceefe52
Rohit Sodhia <sodhia.rohit@gmail.com>
2020-08-10 21:54:29 -0400

    Create first migrations

--- providers/models.py
+++ providers/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class Provider(models.Model):
-    name_given = models.CharField()
-    name_family = models.CharField()
-    title = models.CharField()
-    clinic = models.CharField()
+    name_given = models.CharField(max_length=20)
+    name_family = models.CharField(max_length=20)
+    title = models.CharField(max_length=20)
+    clinic = models.CharField(max_length=40)
     createdAt = models.DateTimeField(auto_now_add=True)
     updatedAt = models.DateTimeField(auto_now=True)
