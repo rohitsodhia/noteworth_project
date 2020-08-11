71ee01fd7558051b7d9853f6222884839bcf272c
Rohit Sodhia <sodhia.rohit@gmail.com>
2020-08-10 22:10:14 -0400

    Create landing app

--- noteworth_project/settings.py
+++ noteworth_project/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     "authtokens.apps.AuthTokensConfig",
     "providers.apps.ProvidersConfig",
+    "landing.apps.LandingConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
--- noteworth_project/urls.py
+++ noteworth_project/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from landing.views import landing as landing_view
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("", landing_view, name="landing"),
+    path("admin/", admin.site.urls),
 ]
