c7fad99b45c12c7c8927f19df3733c7ed6065690
Rohit Sodhia <sodhia.rohit@gmail.com>
2020-08-10 21:42:14 -0400

    Swap authtokens for auth

--- authtokens/models.py
+++ authtokens/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 
 class AuthToken(models.Model):
-    token = models.CharField()
+    token = models.CharField(max_length=20)
     createdAt = models.DateTimeField(auto_now_add=True)
